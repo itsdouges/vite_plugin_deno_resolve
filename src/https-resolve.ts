fbbf2fb13cf47c52ba7c430103be16d73954c35c
DOUGES <itsdouges@users.noreply.github.com>
2022-12-23 13:48:16 +1100

    fix: work around vite parsing paths

--- src/https-resolve.ts
+++ src/https-resolve.ts
@@ -1,6 +1,16 @@
 import { denoCache, denoInfo } from './deno.ts';
 import type { ESModule } from './types.ts';
 
+const DENO_SPECIFIER = 'deno:';
+
+function toHttpsSpecifier(specifier: string) {
+  return specifier.replace(DENO_SPECIFIER, 'https://');
+}
+
+function toDenoSpecifier(specifier: string) {
+  return specifier.replace('https://', DENO_SPECIFIER);
+}
+
 export default function httpsResolve() {
   return {
     name: 'vite:deno-https-resolve',
@@ -15,53 +25,59 @@ export default function httpsResolve() {
       // We want to prepend any https imports with "deno:" so it can be picked
       // up by resolvedId(), else it skips it entirely and is loaded as native esm.
       const replaced = code.replaceAll(
-        'from \'https://',
-        'from \'deno:https://',
+        /from ("|')(https:\/\/.+)("|')/g,
+        (str) => {
+          return toDenoSpecifier(str);
+        },
       );
 
       return replaced;
     },
 
     async resolveId(importee: string, importer: string) {
-      if (importee.indexOf('deno:') === 0) {
+      if (importee.indexOf(DENO_SPECIFIER) === 0) {
         // We have found a top level import for a Deno module.
-        const specifier = importee.replace('deno:', '');
+        const specifier = toHttpsSpecifier(importee);
 
         await denoCache(specifier);
 
-        return specifier;
+        return importee;
       }
 
-      if (importer.indexOf('https://') === 0) {
+      if (importer.indexOf(DENO_SPECIFIER) === 0) {
         // We've found an import inside a deno module, let's find it!
-        const info = await denoInfo(importer);
+        const specifier = toHttpsSpecifier(importer);
+        const info = await denoInfo(specifier);
         const module = info.modules.find((mod): mod is ESModule =>
-          mod.specifier === importer
+          mod.specifier === specifier
         );
         const dependency = module?.dependencies.find((dep) =>
           dep.specifier === importee
         );
 
         if (!dependency || !dependency.code) {
-          throw new Error('invaraint');
+          throw new Error('invaraint: module not found during resolveId()');
         }
 
-        return dependency.code.specifier;
+        return toDenoSpecifier(dependency.code.specifier);
       }
 
       return null;
     },
 
     async load(id: string) {
-      if (id.indexOf('https://') === 0) {
-        const info = await denoInfo(id);
+      if (id.indexOf(DENO_SPECIFIER) === 0) {
+        const specifier = toHttpsSpecifier(id);
+        const info = await denoInfo(specifier);
         const module = info.modules.find((mod): mod is ESModule =>
-          mod.specifier === id
+          mod.specifier === specifier
         );
 
-        if (module) {
-          return await Deno.readTextFile(module.emit);
+        if (!module) {
+          throw new Error('invariant: deno module not found during load()');
         }
+
+        return await Deno.readTextFile(module.emit);
       }
 
       return null;
