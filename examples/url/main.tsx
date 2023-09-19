553c2e8a28870db1270ab92999ad2c08ffbb6a2b
Luke Kaalim <lukekaalim@gmail.com>
2023-09-20 02:46:17 +1000

    Update example to demonstrate feature

--- examples/url/main.tsx
+++ examples/url/main.tsx
@@ -1,4 +1,4 @@
-import React from 'https://esm.sh/react@18.2.0';
+import React from "https://esm.sh/react@18.2.0";
 import { createRoot } from 'https://esm.sh/react-dom@18.2.0/client';
 import { App } from './local.tsx';
 import './style.css';
